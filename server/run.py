48105d5240af88cc67489f945618a7304aa1fe2a
imemmul <emir.ulurak@ozu.edu.tr>
2023-12-26 23:33:50 +0000

    server

--- server/run.py
+++ server/run.py
@@ -5,7 +5,43 @@ import base64
 import cv2
 import numpy as np
 
+import os
+os.environ['LD_LIBRARY_PATH'] = '/usr/lib/arm-linux-gnueabihf'
 connected = set()
+frames = []
+
+def create_video_from_frames(frame_list, output_file='output_video.mp4', codec='mp4v', fps=30):
+    """
+    Create an MP4 video from a list of frames.
+    
+    Args:
+        frame_list (list): List of frames, where each frame is a NumPy array representing an image.
+        output_file (str): Output video file name.
+        codec (str): Codec to use for the video (e.g., 'mp4v', 'XVID', 'H264', etc.).
+        fps (int): Frames per second for the output video.
+
+    Returns:
+        None
+    """
+    # Check if the frame list is not empty
+    if not frame_list:
+        raise ValueError("Frame list is empty")
+
+    # Get frame dimensions from the first frame in the list
+    frame_height, frame_width, _ = frame_list[0].shape
+
+    # Create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*codec)
+    out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
+
+    # Write each frame to the video
+    for frame in frame_list:
+        out.write(frame)
+
+    # Release the VideoWriter when done
+    out.release()
+
+    print(f"Video saved as {output_file}")
 
 def decode_image(base64_image):
     # Decode from base64
@@ -14,6 +50,7 @@ def decode_image(base64_image):
     jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
     # Decode image
     img = cv2.imdecode(jpg_as_np, flags=1)
+    frames.append(img)
     return img
 
 async def handler(websocket, path):
@@ -24,6 +61,7 @@ async def handler(websocket, path):
             data = json.loads(message)
             if "image" in data:
                 # Decode the image
+                print(data)
                 frame = decode_image(data["image"])
                 # Here, you can process the frame as needed
                 print("Frame received and decoded")
@@ -36,6 +74,7 @@ async def handler(websocket, path):
                 print(f"Received non-image data: {data}")
     finally:
         # Unregister and clean up when the connection is closed
+        create_video_from_frames(frame_list=frames, fps=30)
         connected.remove(websocket)
 
 async def main():
@@ -44,3 +83,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
